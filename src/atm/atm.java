c09d10c465380006d954a46e8a49fea9ccdf9028
Asanka301 <111226893+Asanka301@users.noreply.github.com>
2022-08-28 15:01:03 +0530

    Add files via upload

---
+++ src/atm/atm.java
@@ -0,0 +1,11 @@
+package atm;
+
+import java.io.IOException;
+
+public class atm extends OptionMenu{
+    public static void main(String[] args) throws IOException {
+        OptionMenu optionMenu = new OptionMenu();
+
+        optionMenu.getLogin();
+    }
+}
